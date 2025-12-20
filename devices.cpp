1cba16e01b7b8cc9e1c5805661ab733e8ae9f08e
luniphys_lin <luniphys@gmail.com>
2025-12-20 15:02:34 +0100

    created base class

--- devices.cpp
+++ devices.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 
+class Device {
+    private:
+        std::string name;
+    
+    public:
+        Device(std::string name) : name(std::move(name)) {}
+
+        const std::string& getName() const { return name; }
+
+        virtual void printInfo() const {
+            std::cout << "Device Name: " << name << std::endl;
+        }
+};
+
 int main() {
 
-    std::cout << "Hello world." << std::endl;
+    Device device("iphone");
 
     return 0;
 }
